e3f854229efde411d78a749a4f7d78696c1c103d
nicoContigliani <nico.contigliani@gmail.com>
2021-10-05 15:17:50 -0300

    controllers get

--- bkupay/controllers/cardControllers .js
+++ bkupay/controllers/cardControllers .js
@@ -1,7 +1,7 @@
 const pool = require('../databases/database');
 
 const get = async (req, res) => {
-    const response = await pool.query('SELECT * FROM clients ORDER BY  id_client ASC');
+    const response = await pool.query('SELECT * FROM cards ORDER BY  id_card ASC');
     res.status(200).json(response.rows);
 };
 
--- bkupay/controllers/transactionControllers.js
+++ bkupay/controllers/transactionControllers.js
@@ -1,7 +1,7 @@
 const pool = require('../databases/database');
 
 const get = async (req, res) => {
-    const response = await pool.query('SELECT * FROM clients ORDER BY  id_client ASC');
+    const response = await pool.query('SELECT * FROM transactions ORDER BY  id_transaction ASC');
     res.status(200).json(response.rows);
 };
 
