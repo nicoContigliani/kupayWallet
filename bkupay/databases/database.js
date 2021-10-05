f835fee2b83d845de6771dbf30f4fa3bfac05dec
nicoContigliani <nico.contigliani@gmail.com>
2021-10-05 13:58:01 -0300

    server y ruta

---
+++ bkupay/databases/database.js
@@ -0,0 +1,21 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "root",
+  password: "Jesus6388",
+  host: "localhost",
+  port: 5432,
+  database: "kupay"
+});
+pool.connect((error)=>{
+  if (error) {
+    console.error('El error de conexión es: ' + error);
+    return;
+  }
+  console.log('¡Conectado a la Base de Datos!');
+});
+
+
+
+
+module.exports = pool;
---
+++ bkupay/server.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const cors = require("cors");
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+var morgan = require('morgan')
+const pool = require('./databases/database');
+
+const port = 3000
+
+app.use('/', require('./routes'));
+
+
+app.listen(port, () => console.log(`coorriendo por el internet`))
