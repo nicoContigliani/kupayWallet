f3de9ad1d1ba46330c6ea9240265893eed88cdd1
nicoContigliani <nico.contigliani@gmail.com>
2021-10-06 02:50:45 -0300

    wallettioncontrollers

--- bkupay/controllers/walletControllers.js
+++ bkupay/controllers/walletControllers.js
@@ -5,10 +5,55 @@ const get = async (req, res) => {
     res.status(200).json(response.rows);
 };
 
+const getId = async (req, res) => {
+    const id_wallet = parseInt(req.params.id);
+    const response = await pool.query('SELECT * FROM wallets where id_wallet =' + id_waññet);
+    res.status(200).json(response.rows);
+};
+
+
+const save = async (req, res) => {
+    const { id_agent, detail, active } = req.body;
+    const response = await pool.query('INSERT INTO agents (name_wallet, passwords_card, count_wallet) VALUES ($1, $2,$3)', [name_wallet, passwords_card, count_wallet]);
+
+    res.json({
+        message: 'wallet Added successfully',
+        body: {
+            wallet: { name_wallet, passwords_card, count_wallet }
+        }
+    })
+};
+
+const deletes = async (req, res) => {
+    const id_wallet = parseInt(req.params.id);
+
+    await pool.query('DELETE FROM wallets where id_wallet = $1', [
+        id_wallet
+    ]);
+    res.json(`Wallet ${id_wallet} deleted Successfully`);
+};
+
+
+const update = async (req, res) => {
+    // console.log(req.params);
+    // console.log(req.body);
+    const id_wallet = parseInt(req.params.id);
+    console.log(id_wallet);
+    const { name_wallet, passwords_card, count_wallet } = req.body;
+    const response = await pool.query('UPDATE agents SET name_wallet=$1, passwords_card=$2, count_wallet=$3 where id_wallet = $4', [
+        name_wallet, passwords_card, count_wallet, id_wallet
+    ]);
+    res.json(`wallet ${id_wallet} Updated Successfully`);
+};
+
+
+
+
+
 module.exports = {
-     get,
-    // getId,
-    // save,
-    // update,
-    // deletes
+    get,
+    getId,
+    save,
+    update,
+    deletes
 };
--- bkupay/routes/index.js
+++ bkupay/routes/index.js
@@ -16,27 +16,30 @@ router.delete('/card/:id', card.deletes);
 
 router.get('/client', client.get);
 router.get('/client/:id', client.getId);
-router.post('/client',client.save);
-router.post('/client/:id',client.update);
+router.post('/client', client.save);
+router.post('/client/:id', client.update);
 router.delete('/client/:id', client.deletes);
 
 router.get('/product', product.get);
 router.get('/product/:id', product.getId);
-router.post('/product',product.save);
-router.post('/product/:id',product.update);
+router.post('/product', product.save);
+router.post('/product/:id', product.update);
 router.delete('/product/:id', product.deletes);
 
 
 router.get('/transaction', transaction.get);
 router.get('/transaction/:id', transaction.getId);
-router.post('/transaction',transaction.save);
-router.post('/transaction/:id',transaction.update);
-router.delete('/transaction/:id',transaction.deletes);
+router.post('/transaction', transaction.save);
+router.post('/transaction/:id', transaction.update);
+router.delete('/transaction/:id', transaction.deletes);
 
 
 
 router.get('/wallet', wallet.get);
-
+router.get('/wallet/:id', wallet.getId);
+router.post('/wallet', wallet.save);
+router.post('/wallet/:id', wallet.update);
+router.delete('/wallet/:id', wallet.deletes);
 
 
 
